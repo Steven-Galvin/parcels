b90af951a2c6a9916c5b84d2d4a36beb70f891b0
Jennifer Kinsey and Steven Galvin <steven@>
2017-04-24 12:02:00 -0700

    add cost to ship function with passing specs.

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -9,4 +9,8 @@ class Parcel
   define_method(:volume) do
     (@length * @width * @height)
   end
+
+  def cost_to_ship 
+    volume() * @weight * 0.02
+  end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -6,4 +6,9 @@ describe('Parcel#volume') do
     test_parcel = Parcel.new(4, 5, 2, 12)
     expect(test_parcel.volume()).to(eq(40))
   end
+
+  it('takes the volume and the weight and calculates the price to ship') do
+    test_parcel = Parcel.new(4, 5, 2, 12)
+    expect(test_parcel.cost_to_ship()).to(eq(9.60))
+  end
 end
